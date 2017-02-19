39109240555839f9c4004dea7b2b21c7c5c27427
Cornelis Dullemond <dullemond@uni-heidelberg.de>
2017-02-19 20:12:07 +0100

    Added own python Mie code, and moved old fortran Mie codes into a subdirectory. The python Mie code is more flexible and being programmed natively in python it is much easier to handle and to analyze the results. It also now has forward-scattering-chopping for cases of a>>lambda, and a Gaussian size distribution.

--- opac/dust_continuum/jena/configure.h
+++
@@ -1,5 +0,0 @@
-#define DUST_DATABASE_PATH "/Users/dullemon/science/simulations/disk2d/selfshadow_series"
-#define DUST_DATABASE_PATHE "/sources/makeopac/optconst/"
-#define FRSIZE_FREQ           4000
-#define DUST_SPECIES_MAX 1000
-#define DUST_SIZE_MAX 1
